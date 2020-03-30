952a51709efc3fb1d715644ca6ab5a98aaf21493
161250085 <lml19971214>
2020-03-30 18:12:00 +0800

    框架版本

---
+++ src/main/java/cn/edu/nju/tis/TisApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.tis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/nju/tis/TisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.tis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
