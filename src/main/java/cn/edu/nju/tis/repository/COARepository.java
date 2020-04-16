5383d20d7d4a70903fc38a59f7fe6b8ed3896d28
161250085 <lml19971214>
2020-04-16 15:10:56 +0800

    create table cause_of_actions (     coa_id   int auto_increment         primary key,     coa_type enum ('CRIMINAL', 'CIVIL', 'ADMINISTRATIVE') null,     coa_name varchar(255)                                 null,     user_id  varchar(255)                                 null )     comment '案由表，包含案由id（自增），案由名称、还有创建人的id';
    create table coa_info_items
    (
        info_item_id int auto_increment,
        coa_id       int null,
        constraint coa_info_items_pk
            unique (info_item_id),
        constraint coa_info_items_cause_of_actions_coa_id_fk
            foreign key (coa_id) references cause_of_actions (coa_id)
    )
        comment '案由信息表，信息项id与案由id组成主键，其中coa_id是外键';
    
    create table info_items
    (
        info_item_id   int auto_increment
            primary key,
        info_item_name varchar(255) null,
        item_code      text         null,
        user_id        varchar(255) null
    );
    
    create table users
    (
        user_account varchar(255)                 not null
            primary key,
        user_name    varchar(255)                 null,
        user_psw     varchar(255)                 not null,
        user_type    enum ('Manager', 'Ordinary') null
    )
        comment '用户表，type1是管理员，0是普通用户';

---
+++ src/main/java/cn/edu/nju/tis/repository/COARepository.java
@@ -0,0 +1,8 @@
+package cn.edu.nju.tis.repository;
+
+import cn.edu.nju.tis.model.CauseOfAction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface COARepository extends JpaRepository<CauseOfAction, Integer> {
+
+}
--- src/main/java/cn/edu/nju/tis/repository/UserRepository.java
+++ src/main/java/cn/edu/nju/tis/repository/UserRepository.java
@@ -3,6 +3,9 @@ package cn.edu.nju.tis.repository;
 import cn.edu.nju.tis.model.User;
 import cn.edu.nju.tis.utils.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 /**
  * @Author cruck
  * @Description
@@ -13,6 +16,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByAccount(String account);
 
-    User findByType(UserType type);
+    List<User> findByType(UserType type);
+
+    User findUserByName(String name);
+
+    User findUserByNameAndType(String name, UserType type);
 
 }
