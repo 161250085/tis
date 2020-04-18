2c0d4f9bede0d00fa19e29adc7f1cf53026ccf5d
161250085 <lml19971214>
2020-04-18 14:33:34 +0800

    1.新增登录安全检测、密码加密 2.修改了User类的一些属性 3.增加了自动生成Account的工具类 ..。

--- src/main/java/cn/edu/nju/tis/security/WebSecurityConfig.java
+++ src/main/java/cn/edu/nju/tis/security/WebSecurityConfig.java
@@ -2,11 +2,16 @@ package cn.edu.nju.tis.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,33 +25,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/user/**").hasAuthority("USER")
-//                .antMatchers("/manager/**","/import/**").hasAuthority("MANAGER")
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().successHandler(loginAuthenticationSuccessHandler()).loginPage("/login").failureUrl("/login?error=true")
-//                .permitAll()
-//                .and().logout().permitAll().logoutSuccessUrl("/login");
-//
-//        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-//        filter.setEncoding("UTF-8");
-//        filter.setForceEncoding(true);
-//        http.addFilterBefore(filter, CsrfFilter.class);
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userService()).passwordEncoder(passwordEncoder());
-//    }
-//
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().antMatchers("/css/**", "/js/**", "/fonts/**", "/img/**");
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/ordinary/**").hasAuthority("ORDINARY")
+                .antMatchers("/manager/**","/import/**").hasAuthority("MANAGER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().successHandler(loginAuthenticationSuccessHandler()).loginPage("/login").failureUrl("/login?error=true")
+                .permitAll()
+                .and().logout().permitAll().logoutSuccessUrl("/login");
+
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService()).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/fonts/**", "/img/**");
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
