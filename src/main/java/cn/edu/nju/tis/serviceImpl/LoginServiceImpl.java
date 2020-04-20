1fd2f98f40c3b6632187ee0e52ec5e02a2e2d81f
LiuXueYing1127 <1097188600@qq.com>
2020-04-20 22:27:42 +0800

    “登录功能”

---
+++ src/main/java/cn/edu/nju/tis/serviceImpl/LoginServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.edu.nju.tis.serviceImpl;
+
+/**
+ * @ClassName LoginServiceImpl
+ * @Description TODO
+ * @Author liuxueying
+ * @Date 2020/4/18 9:33 PM
+ * @Version 1.0
+ **/
+public class LoginServiceImpl {
+}
---
+++ src/main/java/cn/edu/nju/tis/utils/ResultMessageUtil.java
@@ -0,0 +1,35 @@
+package cn.edu.nju.tis.utils;
+
+import cn.edu.nju.tis.bean.ResultMessageBean;
+
+import static cn.edu.nju.tis.bean.ResultMessageBean.SUCCESS;
+
+/**
+ * @ClassName ResultMessageUtil
+ * @Description TODO
+ * @Author liuxueying
+ * @Date 2020/4/18 9:23 PM
+ * @Version 1.0
+ **/
+public class ResultMessageUtil {
+    //当正确时返回的值
+    public static ResultMessageBean success(Object resultData){
+        ResultMessageBean result = new ResultMessageBean();
+        result.setResultCode(0);
+        result.setResultMessage(SUCCESS);
+        result.setResultData(resultData);
+        return result;
+    }
+
+    public static ResultMessageBean success(){
+        return success(null);
+    }
+
+    //当错误时返回的值
+    public static ResultMessageBean error(int resultCode,String resultMessage){
+        ResultMessageBean result = new ResultMessageBean();
+        result.setResultCode(resultCode);
+        result.setResultMessage(resultMessage);
+        return result;
+    }
+}
